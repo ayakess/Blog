fd521376ec5b4a9b1b428dacf0617c37f3c8e5b5
ayakess <kessay003@gmail.com>
2024-06-07 00:12:21 +0100

    first

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Container, Heading, Text } from '@chakra-ui/react';
 import { FaFileAlt, FaEnvelope } from 'react-icons/fa'; // Importez des icônes pour les liens
+import { NavLink } from 'react-router-dom';
+
+
+
 
 const Home = () => {
     return (
@@ -9,11 +13,14 @@ const Home = () => {
                 Bienvenue sur mon site web
             </Heading>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-                <a href="/cv" className="bg-white shadow-md rounded-lg p-6 hover:shadow-lg transition-shadow duration-300 flex flex-col items-center">
-                    <FaFileAlt size={40} color="#3182CE" />
-                    <h2 className="text-2xl font-bold my-4 text-blue-600">Mon CV</h2>
-                    <p className="text-gray-700 text-center">Consultez mon CV détaillé</p>
-                </a>
+            <NavLink
+            to="/cv"
+            className="bg-white shadow-md rounded-lg p-6 hover:shadow-lg transition-shadow duration-300 flex flex-col items-center"
+        >
+            <FaFileAlt size={40} color="#3182CE" />
+            <h2 className="text-2xl font-bold my-4 text-blue-600">Mon CV</h2>
+            <p className="text-gray-700 text-center">Consultez mon CV détaillé</p>
+        </NavLink>
 
                 <a href="/formulaire" className="bg-white shadow-md rounded-lg p-6 hover:shadow-lg transition-shadow duration-300 flex flex-col items-center">
                     <FaEnvelope size={40} color="#3182CE" />
